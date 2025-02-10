46815174fbde544071ebc6126883b6eed10dadbe
PacificAssistant <gnkh1579@gmail.com>
2025-02-10 15:43:12 +0200

    release 1.0.7

--- database/database.py
+++ database/database.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 
 load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL").replace("postgres://", "postgresql://") if os.getenv("DATABASE_URL") else None
 
 if not DATABASE_URL:
     raise ValueError("❌ DATABASE_URL не знайдено! Перевір змінні середовища.")
